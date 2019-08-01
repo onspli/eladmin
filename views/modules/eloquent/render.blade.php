00d37a880dd1de7e763de4a3c4c34c05b76c6223
Onspli <ondrej.splichal@gmail.com>
2019-08-01 11:55:02 +0200

    update crud render

--- views/modules/eloquent/render.blade.php
+++ views/modules/eloquent/render.blade.php
@@ -327,13 +327,8 @@ $(document).on('click', 'form *[data-elaupdateaction]', function(e){
   elaRequest($(this).data('elaupdateaction'), form.data('elamodule'), form.serialize(), {elaupdate: 1})
   .fail(function(response){
     toastr.error(response.responseText);
-  }).done(function(data, status,xhr){
-    eval('with(data){'+el.data('eladone')+'}');
-    var ct = xhr.getResponseHeader("content-type") || "";
-    if (ct.indexOf('json') > -1) {
-    }else{
-      toastr.success(data?data:'OK');
-    });
+  }).done(function(data){
+    eval('with(data){'+el.data('eladone')+'; toastr.success(data?data:"OK");}');
   }).always(function(){
     elaRequest('putForm', '{{$module->elakey()}}', {
       {{$module->getKeyName()}} : el.data('elaarg{{$module->getKeyName()}}')
